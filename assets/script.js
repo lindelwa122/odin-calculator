f9248778cfcb01507153e36b6b49ca6ad9aae359
Lindelwa <lindelwa.codes@gmail.com>
2023-08-29 08:11:47 +0200

    Add operate
    Add operate so it can be easier to manage the calculator's operations.

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,19 @@ const divide = (a, b) => a / b;
 let numA;
 let operator;
 let numB;
+
+const operate = (numA, numB, operator) => {
+  switch (operator) {
+    case '+':
+      return add(numA, numB);
+
+    case '-':
+      return subtract(numA, numB);
+
+    case '*':
+      return multiply(numA, numB);
+
+    case '/':
+      return divide(numA, numB);
+  }
+};
