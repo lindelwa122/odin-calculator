1b1acb099ae9144c84212b0223f6c1cf42388dfa
Lindelwa <lindelwa.codes@gmail.com>
2023-08-31 13:56:44 +0200

    Finish calculator

--- assets/script.js
+++ assets/script.js
@@ -9,135 +9,200 @@ let numB;
 
 const operate = (numA, numB, operator) => {
   switch (operator) {
-    case '+':
+    case "+":
       return add(numA, numB);
 
-    case '-':
+    case "-":
       return subtract(numA, numB);
 
-    case '*':
+    case "*":
       return multiply(numA, numB);
 
-    case '/':
+    case "/":
       return divide(numA, numB);
   }
 };
 
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
-  button.addEventListener('click', () => controller(button.dataset.value));
-})
+  button.addEventListener("click", () => controller(button.dataset.value));
+});
 
 const cleanOperationsArray = (array) => {
   for (let i = 0; i <= array.length - 1; i++) {
-    array[i] = array[i].replaceAll(',', '');
+    array[i] = array[i].replaceAll(",", "");
   }
 
   return array;
-}
+};
 
 let displayValue = [];
 
 const clearScreen = () => {
-  const inputDisplay = document.querySelector('.input');
-  inputDisplay.value = '';
+  const inputDisplay = document.querySelector(".input");
+  inputDisplay.value = "";
 
-  const currentResultDisplay = document.querySelector('.current-answer');
+  const currentResultDisplay = document.querySelector(".current-answer");
   currentResultDisplay.textContent = 0;
 
   displayValue = [];
+};
+
+const isFloat = (num) => {
+  return !(+num % 2 === 0 || +num % 2 === 1);
 }
 
 const getFinalAnswer = () => {
-  const inputDisplay = document.querySelector('.input');
+  const inputDisplay = document.querySelector(".input");
 
   const cleanArray = cleanOperationsArray(displayValue);
-  const answer =  getCurrentAnswer(cleanArray)
+  const answer = getCurrentAnswer(cleanArray);
+
   inputDisplay.value = formatDigits(answer.toString());
 
   displayValue = [answer.toString()];
-  
-  const currentResultDisplay = document.querySelector('.current-answer');
-  currentResultDisplay.textContent = '';
-}
+
+  const currentResultDisplay = document.querySelector(".current-answer");
+  currentResultDisplay.textContent = "";
+};
 
 const populateDisplay = () => {
-  const inputDisplay = document.querySelector('.input');
+  const inputDisplay = document.querySelector(".input");
 
   const tempArray = [];
   for (let i = 0; i <= displayValue.length - 1; i++) {
     if (i % 2 === 0) {
-      const formattedDigit = formatDigits(displayValue[i]);
-      tempArray.push(formattedDigit);
+      if (displayValue[i].includes('.')) {
+        tempArray.push(displayValue[i]);
+      } else {
+        const formattedDigit = formatDigits(displayValue[i]);
+        tempArray.push(formattedDigit);
+      }
     } else {
       tempArray.push(displayValue[i]);
     }
   }
 
-  inputDisplay.value = tempArray.join('');
+  inputDisplay.value = tempArray.join("");
 
   // only update if a number was typed
   // (by logic the array's length must be odd for it to end with a number)
   if (displayValue.length % 2 === 1) {
-    const currentResultDisplay = document.querySelector('.current-answer');
-    currentResultDisplay.textContent = getCurrentAnswer(displayValue);
+    const currentResultDisplay = document.querySelector(".current-answer");
+    const answer = getCurrentAnswer(displayValue)
+    currentResultDisplay.textContent = formatDigits(answer.toString());
   }
-}
+};
+
+const validateOperations = (input) => {
+  const operators = ["*", "/", "+", "-"];
+  const arrLength = displayValue.length;
+
+  if (input === '=' && arrLength % 2 === 0) return;
+
+  // prevent double '.' in a number
+  if (input === '.' && displayValue[arrLength - 1].includes('.')) return false;
+
+  // operations [*, /, +, %] cannot be used as the first value
+  if (arrLength === 0 && (input === "*" || input === "/" || input === "+" || input === '%'))
+    return false;
+
+  // operators cannot be used it the two preceding values were operators
+  if (operators.includes(input) && operators.includes(displayValue[arrLength - 1].trim()) && operators.includes(displayValue[arrLength - 2].trim())) return false;
+
+  // operator '-' can not be used twice in a row
+  if (input === "-" && (displayValue[0] === "-" || displayValue[arrLength - 1] === "-"))
+    return false;
+
+  // prevent '.' if no number has been typed
+  if (input === '.' && operators.includes(displayValue[arrLength - 1].trim())) return false;
+
+
+  return true;
+};
 
 const controller = (input) => {
   // recieve an input (button clicked) and responds accordingly
 
+  debugger;
+  const isValid = validateOperations(input);
+  if (!isValid) return;
+
   switch (input) {
-    case 'AC':
+    case "AC":
       clearScreen();
       break;
 
-    case 'undo':
+    case "undo":
       undo();
+      populateDisplay();
       break;
 
-    case '=':
+    case "=":
       getFinalAnswer();
       break;
 
-    case '%':
+    case "%":
       getPercentage();
       populateDisplay();
       break;
 
-    case '+':
-    case '-':
-    case '*':
-    case '/':
-      displayValue.push(` ${input} `);
+    case "-":
+      if (displayValue.length % 2 === 0) {
+        displayValue.push(input);
+        populateDisplay();
+        break;
+      }
+    case "+":
+    case "*":
+    case "/":
+      if (displayValue.length % 2 === 0) {
+        displayValue[displayValue.length - 1] = ` ${input} `;
+      } else {
+        displayValue.push(` ${input} `);
+      }
       populateDisplay();
       break;
-    
-    case '1':
-    case '2':
-    case '3':
-    case '4':
-    case '5':
-    case '6':
-    case '7':
-    case '8':
-    case '9':
-    case '0':
+
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+    case "0":
       appendDigit(input);
       populateDisplay();
       break;
+
+    case ".":
+      displayValue[displayValue.length - 1] += input;
+      populateDisplay();
+      break;
   }
-}
+};
 
 const getPercentage = () => {
-  const lastIndex = displayValue.length > 1 ? displayValue.length - 1 : 0;
-  displayValue[lastIndex] = +displayValue[lastIndex] / 100;
-}
+  const lastIndex = displayValue.length - 1;
+  displayValue[lastIndex] = (+displayValue[lastIndex] / 100).toString();
+};
 
 const undo = () => {
-  const joined = displayValue.join('');
-  displayValue = joined.slice(0, joined.length - 1).split(' ');
-}
+  const arrLength = displayValue.length;
+  const lastElement = 5;
+
+  if (arrLength % 2 === 0) {
+    displayValue.splice(arrLength - 1, 1);
+  } else if (displayValue[arrLength - 1].length === 1) {
+      displayValue.pop();
+  } else {
+    const lastElement = displayValue[arrLength - 1];
+    displayValue[arrLength - 1] = lastElement.slice(0, lastElement.length - 1);
+  }
+};
 
 const appendDigit = (digit) => {
   const lastIndex = displayValue.length - 1;
@@ -149,26 +214,36 @@ const appendDigit = (digit) => {
   } else {
     displayValue[lastIndex] = displayValue[lastIndex] + digit;
   }
-}
+};
 
-const getCurrentAnswer = (array, previousAnswer=0) => {
+const getCurrentAnswer = (array, previousAnswer = 0) => {
   if (array.length % 2 === 0 || array.length <= 1) return previousAnswer;
   const newArrayCopy = array.slice(3);
 
-  // trim operator (array[1]) because by default it has trailing 
+  // trim operator (array[1]) because by default it has trailing
   // space for display purposes
   const answer = operate(+array[0], +array[2], array[1].trim());
   newArrayCopy.unshift(answer);
   return getCurrentAnswer(newArrayCopy, answer);
-}
+};
 
 const formatDigits = (digits) => {
   if (digits.length <= 3) return digits;
 
+  if (isFloat(digits)) {
+    return Math.fround(+digits).toPrecision(5);
+  }
+
+  let isNegative = false;
+  if (digits[0] === "-") {
+    isNegative = true;
+    digits = digits.slice(1);
+  }
+
   const splittedDigits = [];
 
   for (let i = 0; i < Math.floor(digits.length / 3); i++) {
-    const end = digits.length - (i*3);
+    const end = digits.length - i * 3;
     const start = end - 3;
     const temp = digits.slice(start, end);
     splittedDigits.unshift(temp);
@@ -177,6 +252,10 @@ const formatDigits = (digits) => {
   if (digits.length % 3 !== 0 && digits.length > 3) {
     splittedDigits.unshift(digits.slice(0, digits.length % 3));
   }
-  
+
+  if (isNegative) {
+    return "-" + splittedDigits.join();
+  }
+
   return splittedDigits.join();
-} 
+};
